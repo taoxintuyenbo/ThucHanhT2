a19b3585a16d592e13604f759d1bba9b76af6f5b
taoxintuyenbo <145319703+taoxintuyenbo@users.noreply.github.com>
2023-10-09 11:21:37 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿
+namespace KTCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.MaNV = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.TenNV = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Tuoi = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.GioiTinh = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtGioiTinh = new System.Windows.Forms.ComboBox();
+            this.txtTuoi = new System.Windows.Forms.TextBox();
+            this.txtTenNV = new System.Windows.Forms.TextBox();
+            this.txtMaNV = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.btnFile = new System.Windows.Forms.Button();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(580, 211);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 31);
+            this.btnThoat.TabIndex = 11;
+            this.btnThoat.Text = "Thoat";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(271, 211);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 31);
+            this.btnXoa.TabIndex = 9;
+            this.btnXoa.Text = "Xoa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(118, 211);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 31);
+            this.btnThem.TabIndex = 8;
+            this.btnThem.Text = "Them";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(422, 211);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 31);
+            this.btnSua.TabIndex = 10;
+            this.btnSua.Text = "Sua";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listView1);
+            this.groupBox2.Location = new System.Drawing.Point(48, 268);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(704, 195);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Thong Tin Nhan Vien";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.MaNV,
+            this.TenNV,
+            this.Tuoi,
+            this.GioiTinh});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(20, 40);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(667, 135);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.Click += new System.EventHandler(this.listView1_Click);
+            // 
+            // MaNV
+            // 
+            this.MaNV.Text = "MaNV";
+            this.MaNV.Width = 90;
+            // 
+            // TenNV
+            // 
+            this.TenNV.Text = "TenNV";
+            this.TenNV.Width = 90;
+            // 
+            // Tuoi
+            // 
+            this.Tuoi.Text = "Tuoi";
+            this.Tuoi.Width = 90;
+            // 
+            // GioiTinh
+            // 
+            this.GioiTinh.Text = "GioiTinh";
+            this.GioiTinh.Width = 90;
+            // 
+            // txtGioiTinh
+            // 
+            this.txtGioiTinh.FormattingEnabled = true;
+            this.txtGioiTinh.Items.AddRange(new object[] {
+            "Nam",
+            "Nu"});
+            this.txtGioiTinh.Location = new System.Drawing.Point(477, 102);
+            this.txtGioiTinh.Name = "txtGioiTinh";
+            this.txtGioiTinh.Size = new System.Drawing.Size(194, 28);
+            this.txtGioiTinh.TabIndex = 7;
+            // 
+            // txtTuoi
+            // 
+            this.txtTuoi.Location = new System.Drawing.Point(477, 53);
+            this.txtTuoi.Name = "txtTuoi";
+            this.txtTuoi.Size = new System.Drawing.Size(194, 26);
+            this.txtTuoi.TabIndex = 6;
+            // 
+            // txtTenNV
+            // 
+            this.txtTenNV.Location = new System.Drawing.Point(139, 102);
+            this.txtTenNV.Name = "txtTenNV";
+            this.txtTenNV.Size = new System.Drawing.Size(194, 26);
+            this.txtTenNV.TabIndex = 5;
+            // 
+            // txtMaNV
+            // 
+            this.txtMaNV.Location = new System.Drawing.Point(139, 53);
+            this.txtMaNV.Name = "txtMaNV";
+            this.txtMaNV.Size = new System.Drawing.Size(194, 26);
+            this.txtMaNV.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(400, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Gioi Tinh";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(400, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tuoi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "TenNV";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MaNV";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtGioiTinh);
+            this.groupBox1.Controls.Add(this.txtTuoi);
+            this.groupBox1.Controls.Add(this.txtTenNV);
+            this.groupBox1.Controls.Add(this.txtMaNV);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(48, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(704, 167);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nhap Thong Tin Nhan Vien";
+            // 
+            // pbImage
+            // 
+            this.pbImage.Location = new System.Drawing.Point(798, 33);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(149, 155);
+            this.pbImage.TabIndex = 12;
+            this.pbImage.TabStop = false;
+            // 
+            // btnFile
+            // 
+            this.btnFile.Location = new System.Drawing.Point(830, 208);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(94, 34);
+            this.btnFile.TabIndex = 13;
+            this.btnFile.Text = "Tai Anh";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(975, 491);
+            this.Controls.Add(this.btnFile);
+            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox txtGioiTinh;
+        private System.Windows.Forms.TextBox txtTuoi;
+        private System.Windows.Forms.TextBox txtTenNV;
+        private System.Windows.Forms.TextBox txtMaNV;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader MaNV;
+        private System.Windows.Forms.ColumnHeader TenNV;
+        private System.Windows.Forms.ColumnHeader Tuoi;
+        private System.Windows.Forms.ColumnHeader GioiTinh;
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.Button btnFile;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace KTCSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string ckn = @"Data Source=LAPTOP-5Q8BC9UT\SQLEXPRESS;Initial Catalog=KTCSharp;Integrated Security=True";
+        string sql;
+        SqlConnection kn;
+        SqlCommand th;
+        SqlDataReader ddl;
+        int i = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            kn = new SqlConnection(ckn);
+            hienthi();
+        }
+
+        public void hienthi()
+        {
+            listView1.Items.Clear();
+            kn.Open();
+            sql = @"select MaNV,TenNV,Tuoi,GioiTinh from QLNV";
+            th = new SqlCommand(sql, kn);
+            ddl = th.ExecuteReader();
+            i = 0;
+            while(ddl.Read())
+            {
+                listView1.Items.Add(ddl[0].ToString());
+                listView1.Items[i].SubItems.Add(ddl[1].ToString());
+                listView1.Items[i].SubItems.Add(ddl[2].ToString());
+                listView1.Items[i].SubItems.Add(ddl[3].ToString());
+                i++;
+            }
+            kn.Close();
+
+        }
+
+        private bool kiemTraDuLieu()
+        {
+            if (string.IsNullOrWhiteSpace(txtMaNV.Text))
+            {
+                MessageBox.Show("Ma nhan vien khong the bo trong.", "Loi!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            if (!txtMaNV.Text.Any(char.IsDigit))
+            {
+                MessageBox.Show("Ma nhan vien phai la mot so.", "Loi!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(txtTenNV.Text))
+            {
+                MessageBox.Show("Ten nhan vien khong the bo trong.", "Loi!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+           
+            if(txtTenNV.Text.Any(char.IsDigit))
+            {
+                MessageBox.Show("Ten nhan vien khong the co so.", "Loi!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            if (!int.TryParse(txtTuoi.Text, out int tuoi) || tuoi <= 0)
+            {
+                MessageBox.Show("Tuoi khong hop le. Hay nhap tuoi la mot so nguyen duong.", "Loi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            string gioiTinh = txtGioiTinh.Text.Trim().ToLower();
+            if (gioiTinh != "nam" && gioiTinh != "nu" && gioiTinh != "Nu" && gioiTinh != "Nam")
+            {
+                MessageBox.Show("Gioi tinh khong hop le. Nhap gioi tinh la \"Nam\" hoac \"Nu\".", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private bool MaNVDuyNhat(string MaNV)
+        {
+            foreach (ListViewItem item in listView1.Items)
+            {
+                if (MaNV == item.SubItems[0].Text)
+                {
+                    MessageBox.Show("Ma nhan vien da ton tai.", "Loi!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void UpAnh(string MaNV)
+        {
+            foreach (ListViewItem item in listView1.Items)
+            {
+                if (MaNV == item.SubItems[0].Text)
+                {
+                    pbImage.SizeMode = PictureBoxSizeMode.StretchImage;
+                    string imagePath = @"d:\taoxintuyenbo\anh\"+MaNV+".jpg" ;
+                    pbImage.ImageLocation = imagePath;
+                }
+
+            }
+        }
+
+        private void listView1_Click(object sender, EventArgs e)
+        {
+            txtMaNV.Text = listView1.SelectedItems[0].SubItems[0].Text;
+            txtTenNV.Text = listView1.SelectedItems[0].SubItems[1].Text;
+            txtTuoi.Text = listView1.SelectedItems[0].SubItems[2].Text;
+            txtGioiTinh.Text = listView1.SelectedItems[0].SubItems[3].Text;
+            UpAnh(txtMaNV.Text);
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            if(kiemTraDuLieu()&& MaNVDuyNhat(txtMaNV.Text))
+            {
+                listView1.Items.Clear();
+                kn.Open();
+                sql = @"INSERT INTO QLNV(MaNV, TenNV, Tuoi, GioiTinh)
+            VALUES(N'" + txtMaNV.Text + @"',
+                    N'" + txtTenNV.Text + @"',
+                    N'" + txtTuoi.Text + @"', 
+                    N'" + txtGioiTinh.Text + @"')";
+                th = new SqlCommand(sql, kn);
+                th.ExecuteNonQuery();
+                kn.Close();
+                hienthi();
+            }
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+           if(kiemTraDuLieu())
+            {
+                listView1.Items.Clear();
+                kn.Open();
+                sql = @"UPDATE QLNV
+                    SET TenNV = N'" + txtTenNV.Text + @"',
+                        Tuoi = N'" + txtTuoi.Text + @"',
+                        GioiTinh = N'" + txtGioiTinh.Text + @"'
+                    WHERE (MaNV = N'" + txtMaNV.Text + @"')";
+                th = new SqlCommand(sql, kn);
+                th.ExecuteNonQuery();
+                kn.Close();
+                hienthi();
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            kn.Open();
+            sql = @"Delete FROM QLNV Where (MaNV=N'"+txtMaNV.Text+@"')";
+            th = new SqlCommand(sql, kn);
+            th.ExecuteNonQuery();
+            kn.Close();
+            hienthi();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult f = MessageBox.Show("Ban co muon thoat khong?", "Thong bao", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if(f == DialogResult.Yes)
+            {
+                this.Close();
+                Application.Exit();
+            }
+        }
+
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            pbImage.SizeMode = PictureBoxSizeMode.StretchImage;
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "Open Image";
+            dlg.Filter = "JPEG files (*.jpg)|*.jpg";
+            if(dlg.ShowDialog()==DialogResult.OK)
+            {
+                pbImage.ImageLocation = dlg.FileName;
+            }
+        }
+    }
+}
